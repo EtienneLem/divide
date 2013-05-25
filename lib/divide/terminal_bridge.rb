a9660021f0253ff86c680210d03e8a2431348641
Etienne Lemay <etiennelemay21@gmail.com>
2013-05-25 14:57:11 -0400

    Add iTerm support

--- lib/divide/terminal_bridge.rb
+++ lib/divide/terminal_bridge.rb
@@ -1,18 +1,38 @@
 class Divide::TerminalBridge
+  APP_SUPPORTED = %w(terminal iterm)
+
   def apple_script(cmd)
     cmd = cmd.join("' -e '") if cmd.is_a?(Array)
     `osascript -e '#{cmd}'`
   end
 
-  def tell_terminal(cmd)
-    %(tell app "Terminal" to #{cmd})
+  def tell_current_application(cmd)
+    %(tell app "#{current_app}" to #{cmd})
+  end
+
+  def current_app
+    @current_app_name ||= apple_script('tell app "System Events" to get name of the first process whose frontmost is true').strip
+  end
+
+  def current_app_supported?
+    APP_SUPPORTED.include?(current_app.downcase)
   end
 
   def do_script(script)
-    tell_terminal %(do script "#{script}" in front window)
+    case current_app.downcase
+    when 'terminal' then tell_current_application %(do script "#{script}" in front window)
+    when 'iterm' then tell_current_application %(tell the first terminal to tell the last session to write text "#{script}")
+    end
   end
 
-  def keystroke(app, key)
+  def open_new_tab
+    case current_app.downcase
+    when 'terminal' then keystroke 'command+t'
+    when 'iterm' then tell_current_application %(tell the first terminal to launch session "Default Session")
+    end
+  end
+
+  def keystroke(key)
     splits = key.split('+')
 
     if splits.length > 1
@@ -24,17 +44,17 @@ class Divide::TerminalBridge
     end
 
     modifier = modifier_key ? " using #{modifier_key} down" : ''
-    %(tell app "System Events" to tell process "#{app}" to keystroke "#{key}"#{modifier})
+    %(tell app "System Events" to tell process "#{current_app}" to keystroke "#{key}"#{modifier})
   end
 
   def exec(commands)
     scripts = commands.map { |c| do_script(c) }
-    scripts_with_new_tabs = insert_between(scripts, keystroke('Terminal', 'command+t'))
+    scripts_with_new_tabs = insert_between(scripts, open_new_tab)
 
     apple_script(scripts_with_new_tabs)
   end
 
-  def insert_between(scripts, insert_between)
-    scripts.flat_map { |s| [s, insert_between] }[0...-1]
+  def insert_between(array, insert_between)
+    array.flat_map { |a| [a, insert_between] }[0...-1]
   end
 end
