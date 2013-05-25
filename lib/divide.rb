1f20cc7c59df40213529e5cf69ade1a90aaa8179
Etienne Lemay <etiennelemay21@gmail.com>
2013-05-25 14:57:45 -0400

    Update errors. Exit if app is not supported

--- lib/divide.rb
+++ lib/divide.rb
@@ -5,17 +5,24 @@ require 'divide/terminal_bridge'
 module Divide
   def self.run(argv)
     @options = argv.each_slice(2).to_a
+    error(:app_not_supported) unless terminal.current_app_supported?
+
     terminal.exec(processes)
     1
   end
 
   def self.processes
-    no_profile unless extracted_processes = extractor.extract_processes!
+    error(:no_profile) unless extracted_processes = extractor.extract_processes!
     extracted_processes.to_a.map { |a| a[1] }
   end
 
-  def self.no_profile
-    puts "#{Dir.pwd}: There is no Procfile in this directory"
+  def self.error(type)
+    errors = {
+      no_profile: "#{Dir.pwd}: There is no Procfile in this directory",
+      app_not_supported: "#{terminal.current_app} is not yet supported, please fill in a request https://github.com/EtienneLem/divide/issues",
+    }
+
+    puts errors[type]
     exit
   end
 
