527237308be683d65e74234c332ed6fbe13cd5ac
Etienne Lemay <etiennelemay21@gmail.com>
2013-05-25 19:31:02 -0400

    Fix a bug where extractor would break when option isn’t in the Procfile

--- lib/divide/extractor.rb
+++ lib/divide/extractor.rb
@@ -3,15 +3,18 @@ require 'yaml'
 module Divide
   class Extractor
     def initialize(options=[])
-      @procfile_content = File.read('./Procfile') rescue ''
       @options = options
 
       overwrite_options
       overwrite_port
     end
 
+    def procfile_content
+      @procfile_content ||= File.read('./Procfile') rescue ''
+    end
+
     def overwrite_options
-      splitted_procfile = @procfile_content.split(/\s/)
+      splitted_content = procfile_content.split(/\s/)
 
       @options.each do |option|
         next if option.length < 2
@@ -19,20 +22,20 @@ module Divide
         key = option[0]
         value = option[1]
 
-        key_index = splitted_procfile.index(key)
-        value_to_overwrite = splitted_procfile[key_index + 1]
-
-        @procfile_content.sub!(value_to_overwrite, value)
+        if key_index = splitted_content.index(key)
+          value_to_overwrite = splitted_content[key_index + 1]
+          procfile_content.sub!(value_to_overwrite, value)
+        end
       end
     end
 
     def overwrite_port
-      @procfile_content.sub!('$PORT', ENV['PORT'] || '5000')
+      procfile_content.sub!('$PORT', ENV['PORT'] || '5000')
     end
 
     def extract_processes!
-      return nil if @procfile_content.empty?
-      YAML.load(@procfile_content)
+      return nil if procfile_content.empty?
+      YAML.load(procfile_content)
     end
   end
 end
