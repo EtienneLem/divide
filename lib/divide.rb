bfc76ff2104dd658b28b5fc449d6343bddbf30fd
Etienne Lemay <etiennelemay21@gmail.com>
2013-05-25 01:36:55 -0400

    Overwrite Procfile options/flags i.e. web: rails s -p $PORT
    `divide -p 1337` => `rails s -p 1337`

--- lib/divide.rb
+++ lib/divide.rb
@@ -3,20 +3,21 @@ require 'divide/extractor'
 require 'divide/terminal_bridge'
 
 module Divide
-  def self.terminal
-    @terminal ||= Divide::TerminalBridge.new
-  end
-
-  def self.extractor
-    @extractor ||= Divide::Extractor.new
-  end
-
-  def self.run
+  def self.run(argv)
+    @options = argv.each_slice(2).to_a
     terminal.exec(processes)
     1
   end
 
   def self.processes
     extractor.extract_processes!.to_a.map { |a| a[1] }
+  end
+
+  def self.terminal
+    @terminal ||= Divide::TerminalBridge.new
+  end
+
+  def self.extractor
+    @extractor ||= Divide::Extractor.new(@options)
   end
 end
--- lib/divide/extractor.rb
+++ lib/divide/extractor.rb
@@ -1,11 +1,22 @@
 require 'yaml'
 
 class Divide::Extractor
-  def initialize
-    @procfile = File.read('./Procfile')
+  def initialize(options=[])
+    @procfile_content = File.read('./Procfile')
+    splitted_procfile = @procfile_content.split(/\s/)
+
+    options.each do |option|
+      key = option[0]
+      value = option[1]
+
+      key_index = splitted_procfile.index(key)
+      value_to_overwrite = splitted_procfile[key_index + 1]
+
+      @procfile_content.sub!(value_to_overwrite, value)
+    end
   end
 
   def extract_processes!
-    YAML.load(@procfile)
+    YAML.load(@procfile_content)
   end
 end
