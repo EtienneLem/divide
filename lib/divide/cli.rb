923637ed41b917b03d18c0621beb8f0a43a1996c
Etienne Lemay <etiennelemay21@gmail.com>
2013-05-27 11:41:40 -0400

    Update Divide::CLI

--- lib/divide/cli.rb
+++ lib/divide/cli.rb
@@ -1,39 +1,54 @@
 module Divide
   class CLI
-    def initialize(argv)
+    attr_reader :options
+
+    def initialize(argv=[])
       @options = argv.each_slice(2).to_a
 
       show_version if argv.grep(/^-v|--version$/).any?
       error(:app_not_supported) unless terminal
 
+      processes = extract_processes
+      start_processes(processes)
+    end
+
+    def start_processes(processes)
       terminal.exec(processes)
     end
 
-    def processes
-      error(:no_profile) unless extracted_processes = extractor.extract_processes!
+    def extract_processes
+      error(:no_procfile) unless extracted_processes
       extracted_processes.to_a.map { |a| a[1] }
     end
 
     def error(type)
       errors = {
-        no_profile: "#{Dir.pwd}: There is no Procfile in this directory",
+        no_procfile: "#{current_directory}: There is no Procfile in this directory",
         app_not_supported: "#{current_app_name} is not yet supported, please fill in a request https://github.com/EtienneLem/divide/issues",
       }
 
-      exit_with_message(errors[type])
+      exit_with_message(errors[type], 1)
     end
 
     def show_version
       exit_with_message("Divide #{VERSION}")
     end
 
-    def exit_with_message(message)
+    def exit_with_message(message, code=0)
       STDOUT.puts(message)
-      exit 0
+      exit code
     end
 
     def current_app_name
       @current_app_name ||= TerminalBridge.current_app_name
+    end
+
+    def extracted_processes
+      @extracted_processes ||= extractor.extract_processes!
+    end
+
+    def current_directory
+      @current_directory ||= Dir.pwd
     end
 
     def terminal
